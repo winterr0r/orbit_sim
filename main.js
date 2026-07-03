cb3ecb270ac962f19ac5939fa914cfa1eed58b9d
C0rabeLL <mushroomwlm@gmail.com>
2026-07-03 16:28:55 -0400

    fix redundancy

--- main.js
+++ main.js
@@ -10,19 +10,6 @@ const { scene, camera, renderer, controls } = createScene()
 const G = 1
 let dt = 0.02
 
-//sun and planet
-const sun  = new Body(1000, new THREE.Vector3(0, 0, 0), 0xffaa00)
-const planet = new Body(80, new THREE.Vector3(40, 0, 0), 0x3399ff)
-planet.velocity = new THREE.Vector3(0, 5, 0)
-const satellite = new Body(0.5, new THREE.Vector3(44, 0, 0), 0xfff4e0)
-satellite.velocity = new THREE.Vector3(0, 5 + 4.47, 0)
-
-const bodies = [sun, planet, satellite]
-
-scene.add(sun.mesh)
-scene.add(planet.mesh)
-scene.add(satellite.mesh)
-
 function loadPreset(name) {
   for (const body of bodies) scene.remove(body.mesh)
   bodies.length = 0
